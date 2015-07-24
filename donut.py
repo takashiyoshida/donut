8fce13a7d7a80b814ce0b89bb70281e873e4aabc
t0062248 <takashi.yoshida@asia.thalesgroup.com>
2015-07-24 11:21:16 +0800

    Send Jenkins job status to pushover.net

--- donut.py
+++ donut.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
 import json
 from pprint import pprint
+import time
 import urllib
 import urllib2
 
@@ -36,5 +38,41 @@ def send_notification(message):
     pprint(result.getcode())
 
 
+def get_jenkins_job_status(job):
+    url = "%s/%s/%s" % (JENKINS_URL, job, URL_SUFFIX)
+
+    proxies = {}
+    proxy_handler = urllib2.ProxyHandler({})
+    opener = urllib2.build_opener(proxy_handler)
+
+    data = None
+    try:
+        request = urllib2.Request(url)
+        response = opener.open(request)
+        data = json.loads(response.read())
+        pprint(data)
+    except urllib2.HTTPError as e:
+        pprint(str(e))
+        pprint(url)
+    return data
+
+
 if __name__ == "__main__":
-    send_notification('This is a test message')
+    #send_notification('This is a test message')
+    proxies = {}
+    proxy_handler = urllib2.ProxyHandler({})
+    opener = urllib2.build_opener(proxy_handler)
+
+    JENKINS_URL = 'http://10.216.10.221:8888/jenkins/view/C755A/job'
+    URL_SUFFIX = 'lastBuild/api/json'
+
+    # Add more jobs later
+    JOBS = ['C755A-Build-DatabaseRpmLinux']
+
+    for job in JOBS:
+        data = get_jenkins_job_status(job)
+        if data:
+            duration = timedelta(seconds = data['duration'] / 1000)
+            completeAt = datetime.fromtimestamp(data['timestamp'] / 1000)
+            message = "%s:\n%s at %s" % (data['fullDisplayName'], data['result'], completeAt)
+            send_notification(message)
