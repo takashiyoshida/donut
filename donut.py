0bed0e0c7c09fa6f898b8a654c584f65a8b2bca5
t0062248 <takashi.yoshida@asia.thalesgroup.com>
2015-07-24 10:47:00 +0800

    Refactor code

--- donut.py
+++ donut.py
@@ -5,31 +5,36 @@ from pprint import pprint
 import urllib
 import urllib2
 
+def send_notification(message):
+    with open('donut.json', 'r') as config:
+        data = json.load(config)
+        #pprint(data)
+
+    proxies = {}
+    
+    if data['proxy']['enabled'] == "1":
+        proxy_auth_url = "http://%s:%s@%s:%s" % (data['proxy']['username'],
+                                                 data['proxy']['password'],
+                                                 data['proxy']['url'],
+                                                 data['proxy']['port'])
+        pprint(proxy_auth_url)
+        proxies = {'http': proxy_auth_url,
+                   'https': proxy_auth_url}
+
+    proxy_handler = urllib2.ProxyHandler(proxies)
+    opener = urllib2.build_opener(proxy_handler)
+    payload = urllib.urlencode({'token': data['pushover']['api_token'],
+                                'user': data['pushover']['user_key'],
+                                'message': message, })
+        
+    request = urllib2.Request(data['pushover']['url'], data=payload,
+                              headers={'Content-type': 'application/x-www-form-urlencoded'})
+        
+    result = opener.open(request)
+    pprint(result.geturl())
+    pprint(str(result.info()))
+    pprint(result.getcode())
+
+
 if __name__ == "__main__":
-	with open('donut.json', 'r') as config:
-		data = json.load(config)
-		pprint(data)
-			
-	if data['proxy']['enabled'] == "1":
-		proxy_auth_url = "http://%s:%s@%s:%s" % (data['proxy']['username'],
-												 data['proxy']['password'],
-												 data['proxy']['url'],
-												 data['proxy']['port'])
-		pprint(proxy_auth_url)
-		proxy_handler = urllib2.ProxyHandler({'http': proxy_auth_url,
-		                                      'https': proxy_auth_url})
-	else:
-		proxy_handler = urllib2.ProxyHandler({})
-	
-	opener = urllib2.build_opener(proxy_handler)
-	payload = urllib.urlencode({'token': data['pushover']['api_token'],
-	                            'user': data['pushover']['user_key'],
-	                            'message': 'Hello World', })
-	
-	request = urllib2.Request(data['pushover']['url'], data=payload,
-	                          headers={'Content-type': 'application/x-www-form-urlencoded'})
-	                          
-	result = opener.open(request)
-	pprint(result.geturl())
-	pprint(str(result.info()))
-	pprint(result.getcode())
+    send_notification('This is a test message')
